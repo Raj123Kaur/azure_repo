4d7c0a9aa1bf5c64336fa1b53a04968d87141ca9
Raj123Kaur <49548644+Raj123Kaur@users.noreply.github.com>
2021-03-19 12:17:09 -0700

    Update app.py

--- Audit/app.py
+++ Audit/app.py
@@ -14,8 +14,6 @@ from pykafka import KafkaClient
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-
-
 with open('log_conf.yml', 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
@@ -30,19 +28,21 @@ def get_OrderFood(index):
     topic = client.topics[str.encode(app_config["events"]["topic"])]
     consumer = topic.get_simple_consumer(reset_offset_on_start=True,consumer_timeout_ms=1000)
     logger.info("Retrieving OrderFood at index %d" %index)
+    count = 0
     try:
         for msg in consumer:
             msg_str = msg.value.decode('utf-8')
             msg = json.loads(msg_str)
-            logger.info("Message: %s" % msg)
-            payload = msg["payload"]
-            event= payload.ElementAt(index)
-            return event,200
 
+            if msg["type"] == "OrderFood" and count == index:
+                payload = msg["payload"]
+                return payload, 200
+            if msg["type"] == "OrderFood" and count != index:
+                count= count + 1
     except:
         logger.error("no message found")
 
-    logger.error("could not find OrderFood at index %d" %index)
+    logger.error("could not find OrderFood at index %d" % index)
     return  {"message": "Not Found"}, 404
 
 
@@ -53,21 +53,27 @@ def get_BookRide(index):
     topic = client.topics[str.encode(app_config["events"]["topic"])]
     consumer = topic.get_simple_consumer(reset_offset_on_start=True,consumer_timeout_ms=1000)
     logger.info("Retrieving BookRide at index %d" %index)
+    count = 0
+
     try:
         for msg in consumer:
             msg_str = msg.value.decode('utf-8')
             msg = json.loads(msg_str)
-            logger.info("Message: %s" % msg)
-            payload = msg["payload"]
-            event= payload.ElementAt(index)
-            return event,200
+
+            if msg["type"]=="BookRide" and count==index:
+                payload= msg["payload"]
+                print(payload)
+                return payload, 200
+            if msg["type"]=="BookRide" and count!=index:
+                count=count+1
+
+
 
     except:
         logger.error("no message found")
 
     logger.error("could not find BookRide at index %d" %index)
     return  {"message": "Not Found"}, 404
-
 app= connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
@@ -75,5 +81,3 @@ app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__== "__main__":
     app.run(port=8110)
-
-
