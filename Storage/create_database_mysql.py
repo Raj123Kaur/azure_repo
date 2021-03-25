56954fbec321a3eddfb024b7d34cf95f0af526ca
Raj123Kaur <49548644+Raj123Kaur@users.noreply.github.com>
2021-03-25 12:41:59 -0700

    Update create_database_mysql.py

--- Storage/create_database_mysql.py
+++ Storage/create_database_mysql.py
@@ -9,8 +9,7 @@ db_cursor.execute('''
           pickup_location VARCHAR(250) NOT NULL,
           destination_address VARCHAR(250) NOT NULL,
           pickup_notes VARCHAR(250) NOT NULL, 
-          date_created VARCHAR(100) NOT NULL,
-          CONSTRAINT ride_id_pk PRIMARY KEY (ride_id) )''')
+          date_created VARCHAR(100) NOT NULL )''')
 
 db_cursor.execute('''
           CREATE TABLE order_food
@@ -18,8 +17,7 @@ db_cursor.execute('''
            restaurant_name VARCHAR(250) NOT NULL,
            drop_off_address VARCHAR(250) NOT NULL,
            drop_off_instructions VARCHAR(250) NOT NULL,
-           date_created VARCHAR(100) NOT NULL,
-           CONSTRAINT order_id_pk PRIMARY KEY(order_id)
+           date_created VARCHAR(100) NOT NULL
            )''')
 
 
